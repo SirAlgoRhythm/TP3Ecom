31f46732f5cbcd7fc4b1f052fd7d6d9e7ad3a6ad
MrTognon <thomasarnaud.lys@gmail.com>
2023-11-02 10:55:10 -0400

    STYLE: Ajout de la page d'accueil et bootstrap

--- NoixMagicroquanteWebsite/NoixMagicroquanteWebsite/Program.cs
+++ NoixMagicroquanteWebsite/NoixMagicroquanteWebsite/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMvc(route => route.MapRoute("Default", "{controller=Home}/{action=Index}"));
+
+app.UseFileServer();
 
 app.Run();
