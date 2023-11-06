d2007c74c17b1ac498f9a619d575077c8c52518f
Thomas Lys <100291190+MrTognon@users.noreply.github.com>
2023-11-06 14:16:45 -0500

    FEAT: Ajout de la création de compte (ne fonctionne pas encore)

--- NoixMagicroquanteWebsite/NoixMagicroquanteWebsite/Controllers/AccountController.cs
+++ NoixMagicroquanteWebsite/NoixMagicroquanteWebsite/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NoixMagicroquanteWebsite.Models;
 
 namespace NoixMagicroquanteWebsite.Controllers
 {
     public class AccountController : Controller
     {
+        protected NoixMagicroquanteWebsiteContext db = new NoixMagicroquanteWebsiteContext();
         public IActionResult Index()
         {
             ViewBag.Title = "Noix MagiCroquantes - Compte";
@@ -20,6 +22,37 @@ namespace NoixMagicroquanteWebsite.Controllers
         {
             ViewBag.Title = "Noix MagiCroquantes - Créer un compte";
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.User.FirstOrDefault(u => u.Email == user.Email) == null)
+                {
+                    db.User.Add(user);
+                    db.SaveChanges();
+
+                    TempData["Message"] = "Votre compte a été créé avec succès !";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["Message"] = "La création de votre compte à échouée";
+                    return View(user);
+                }
+            }
+            else
+            {
+                TempData["Message"] = "La création de votre compte à échouée";
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    // Afficher l'erreur dans la console
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            return View(user);
         }
     }
 }
