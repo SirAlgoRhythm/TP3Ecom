86f26b42d24979442d2d2b4ee61c69afe9afd36a
MrTognon <thomasarnaud.lys@gmail.com>
2023-11-10 09:51:11 -0500

    STYLE: Ajout de la possibilite de visualiser le mot de passe ecrit par l'utilisateur en appuyant sur une icone sur la page Login

---
+++ NoixMagicroquanteWebsite/NoixMagicroquanteWebsite/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+﻿function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('Password');
+    var passwordIcon = document.getElementById('passwordIcon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.add('fa-eye-slash');
+        passwordIcon.classList.remove('fa-eye');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.add('fa-eye');
+        passwordIcon.classList.remove('fa-eye-slash');
+    }
+}
